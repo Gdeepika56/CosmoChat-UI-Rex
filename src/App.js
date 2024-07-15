37d7e26886e7dbe4776df92e1959dc7a0629bab6
Gdeepika56 <dg1201@usnh.edu>
2024-07-15 16:39:41 -0400

    make change to app js file

--- src/App.js
+++ src/App.js
@@ -8,38 +8,45 @@ const theme = createTheme();
 
 function App() {
   const [view, setView] = useState('landing');
+  const [activityData, setActivityData] = useState([]);
 
   const startChat = () => {
     setView('chat');
+    addActivity('Chat Started');
+  };
+
+  const endChat = () => {
+    addActivity('Chat Ended');
+    setView('landing');
   };
 
   const showDashboard = () => {
     setView('dashboard');
   };
 
-  const handleChatEnd = () => {
-    setView('landing');
-  };
-
   const handleBackToLanding = () => {
     setView('landing');
   };
 
+  const addActivity = (activity) => {
+    const timestamp = new Date().toLocaleString();
+    setActivityData((prevData) => [...prevData, { activity, timestamp }]);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-      {view === 'landing' && (
-    <div className="landing-page">
-      <h1 className="welcome-heading">Welcome to Chat Service</h1>
-      <div className="button-container">
-        <button className="landing-button" onClick={startChat}>Start Chat</button>
-        <button className="landing-button" onClick={showDashboard}>View Dashboard</button>
-      </div>
-    </div>
-  )}
-        {view === 'chat' && <Chat onChatEnd={handleChatEnd} />}
-        {view === 'dashboard' && <ActivityDashboard onBack={handleBackToLanding} />}
+        {view === 'landing' && (
+          <div className="landing-page">
+            <h1 className="welcome-heading">Welcome to Chat Service</h1>
+            <div className="button-container">
+              <button className="landing-button" onClick={startChat}>Start Chat</button>
+              <button className="landing-button" onClick={showDashboard}>View Dashboard</button>
+            </div>
+          </div>
+        )}
+        {view === 'chat' && <Chat onChatEnd={endChat} />}
+        {view === 'dashboard' && <ActivityDashboard activityData={activityData} onBack={handleBackToLanding} />}
       </div>
     </ThemeProvider>
   );
